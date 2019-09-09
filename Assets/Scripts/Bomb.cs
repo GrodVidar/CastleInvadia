1f0be1cff86c9eb1cf4face80560eba2614c6930
Vidar Franzén <vexxart@gmail.com>
2019-09-09 23:25:25 +0200

    Added Bomb and explosion.

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+    // Start is called before the first frame update
+
+    public void Explode()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+
+    public void KillSelf()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    bool isMovingRight = false;
+    bool isHit = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player" && !isHit)
+        {
+            if(collision.transform.position.x > transform.position.x)
+            {
+                isMovingRight = true;
+            }
+            collision.GetComponent<Player>().ReceiveDamage(25, isMovingRight);
+            isHit = true;
+        }
+        else if(collision.tag == "Enemy" && !isHit)
+        {
+            collision.GetComponent<Enemy>().ReceiveDamage(100);
+            isHit = true;
+        }
+    }
+
+    public void KillSelf()
+    {
+        Destroy(gameObject);
+    }
+}
