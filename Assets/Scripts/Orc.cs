91b9433c13bd8ca046736bb5b6bd545ab041c71f
GrodVidar <vexxart@gmail.com>
2019-09-17 18:33:13 +0200

    Added new Level!! New enemy that is supposed to jump and throw axes?

---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : MonoBehaviour
+{
+	//States
+	bool isFacingRight = true;
+	//Caches
+	Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = FindObjectOfType<Player>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x < transform.position.x && isFacingRight)
+		{
+			Flip();
+			isFacingRight = false;
+		}
+		else if(player.transform.position.x > transform.position.x && !isFacingRight)
+		{
+			Flip();
+			isFacingRight = true;
+		}
+    }
+
+	private void Flip()
+	{
+		transform.localScale = new Vector2(transform.localScale.x * -1, 1f);
+	}
+}
