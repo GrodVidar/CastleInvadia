92f4365d1480d206c5ab45cb8344b641a4d935ac
Vidar Franzén <vexxart@gmail.com>
2019-09-18 08:19:48 +0200

    Working on functions for the Orc

--- Assets/Scripts/Orc.cs
+++ Assets/Scripts/Orc.cs
@@ -8,14 +8,22 @@ public class Orc : MonoBehaviour
 	bool isFacingRight = true;
 	//Caches
 	Player player;
+    BoxCollider2D feet;
     // Start is called before the first frame update
     void Start()
     {
-		player = FindObjectOfType<Player>();   
+		player = FindObjectOfType<Player>();
+        feet = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        LookAtPlayer();
+
+    }
+
+    private void LookAtPlayer()
     {
         if(player.transform.position.x < transform.position.x && isFacingRight)
 		{
@@ -33,4 +41,13 @@ public class Orc : MonoBehaviour
 	{
 		transform.localScale = new Vector2(transform.localScale.x * -1, 1f);
 	}
+
+    private void Jump()
+    {
+        if(feet.IsTouchingLayers(LayerMask.GetMask("Groud")))
+        {
+            Vector2 JumpVelocity = new Vector2(0f, jumpVel);
+            myRigidBody.velocity += JumpVelocity;
+        }
+    }
 }
