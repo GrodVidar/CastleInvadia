af8b16c3afe5ca52ee48a62042b8fe0448671bd9
Vidar Franzén <vexxart@gmail.com>
2019-11-08 16:34:42 +0100

    Unity Changes

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameState : MonoBehaviour
 {
     CashFlow money;
+    bool paused = false;
+    [SerializeField]GameObject pauseCanvas;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +16,27 @@ public class GameState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!paused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            paused = true;
+            pauseCanvas.SetActive(true);
+        }
+        else if(paused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Resume();
+        }
     }
 
-	public void Retry()
+    public void Retry()
 	{
         money.SetMoney(PlayerPrefs.GetInt("Money"));
 		Level level = FindObjectOfType<Level>();
 		level.ReloadLevel();
+        if(paused)
+        {
+            Time.timeScale = 1;
+        }
 	}
 
 	public void QuitGame()
@@ -29,4 +44,13 @@ public class GameState : MonoBehaviour
 		Debug.Log("Quitting");
 		Application.Quit();
 	}
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pauseCanvas.SetActive(false);
+    }
+
+    
 }
